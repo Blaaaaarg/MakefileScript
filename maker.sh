1f3c1652778fb82f2ebe2197f3a526b60eedaf8f
Blaaaaarg <walkesco@seawolf.sonoma.edu>
2014-09-09 22:57:48 -0700

    Updated for 1.4

--- maker.sh
+++ maker.sh
@@ -1,26 +1,16 @@
-# Maker V1.3
+# Maker V1.4
 # Made By: Scott Walker
 # This program generates makefiles for c++ programs
 # For directions see included .md file or go to github.com/Blaaaaarg/MakefileScript
 
-programname="executable"
+programname="executable.x"
+input=""
+version="1.4"
+compiler="g++"
 
-for v in $@
-do
-    if [ "$v" = "-v" ]
-    then
-         verbose="true" 
-    fi
-done
-
-if [ "$verbose" = "true" ]
-then
-    echo -e "Building dependency list..."
-fi
 
 builddeplist()
 {
-
     for filename in *.cpp
     do
         echo $filename | sed 's/.cpp/.o/g'
@@ -47,40 +37,60 @@ hfiles()
 }
 
 
-if [ "$verbose" = "true" ]
-then
-    echo -e "========================"
-    echo -e "User options: "
-fi
-
-for c in $@
+# Check for flags
+for op in $@
 do
-    if [ "$c" = "-c" ]
+    if [ "$op" = "-v" ]
     then
-        compile="true"
-        echo -e "\tEnabled compile by default"
-        echo -e "\tWarning: this will overwrite any executable with the same name"
-    fi
-done
-
-for g in $@
-do
-    if [ "$g" = "-g" ]
+        verbose="true" 
+        echo -e "Maker v$version"
+        echo -e "========================"
+    elif [ "$op" = "-g" ]
     then
         debug="true"
-        echo -e "\tEnabled debugging mode"
-    fi
-done
-
-for m in $@
-do
-    if [ "$m" = "-m" ]
+        if [ "$verbose" = "true" ]
+        then
+            echo -e "\tEnabled debugging mode"
+        fi
+    elif [ "$op" = "-m" ]
     then
         minimal="true"
-        echo -e "\tEnabled minimal mode"
+        if [ "$verbose" = "true" ]
+        then
+            echo -e "\tEnabled minimal mode"
+        fi
+    elif [ "$op" = "-df" ]
+    then
+        compile="true"
+        if [ "$verbose" = "true" ]
+        then
+            echo -e "\tEnabled compile by default (forced)"
+        fi
+    elif [ "$op" = "-gcc" ]
+    then
+        compiler="gcc"
+
+    elif [ "$op" = "-d" ]
+    then
+        compile="true"
+        if [ "$verbose" = "true" ]
+        then
+            echo -e "\tEnabled compile by default"
+        fi
+        if [ "$(find $programname)" = "$programname" ]
+        then
+            echo -e "\tWarning: $programname will be overwritten"
+            echo -e "\tContinue? (y/n)"
+            read input
+            if [ "$input" != "y" ]
+            then
+                exit;
+            fi
+        fi
     fi
 done
 
+# Check for rename (-n) flag
 for name in $@
 do
     if [ "$tag" = "true" ]
@@ -88,7 +98,7 @@ do
         programname="$name"
         if [ "$verbose" = "true" ]
         then
-            echo -e "\tEnabled executable name as '$name'"
+            echo -e "\tWriting executable name as '$name'"
         fi
         break
     fi
@@ -101,29 +111,31 @@ done
 
 if [ "$verbose" = "true" ]
 then
-    echo -e "========================"
-    echo -e "Building the makefile..."
+    echo -e "Using $compiler"
+    echo -e "Building the makefile"
 fi
 
-echo -e "#"Makefile created by maker v1.2"\t"github.com/Blaaaaarg/MakefileScript > makefile
+echo -e "#"Makefile created by maker v$version"\t"github.com/Blaaaaarg/MakefileScript > makefile
 
-echo $programname.x : $(builddeplist) >> makefile
+if [ "$verbose" = "true" ]
+then
+    echo -e "Building dependency list"
+fi
 
+echo $programname : $(builddeplist) >> makefile
+
+# Create makefile commands
 if [ "$debug" = "true" ]
 then
-    echo -n -e "\t"g++ -g -o $programname.x >> makefile
+    echo -n -e "\t"$compiler -g -o $programname >> makefile
 else
-    echo -n -e "\t"g++ -o $programname.x >> makefile
+    echo -n -e "\t"$compiler -o $programname >> makefile
 fi
 
 echo -n -e " " $(builddeplist) >> makefile
 echo -n -e "\n" >> makefile
 
-if [ "$verbose" = "true" ]
-then
-    echo -n "."
-fi
-
+# Find dependancies for all project files
 for filename in *.cpp
 do
     echo -n $filename | sed 's/.cpp/.o/g' >> makefile
@@ -131,21 +143,17 @@ do
     echo -e " " $(hfiles) >> makefile
     if [ "$debug" = "true" ]
     then
-        echo -n -e "\t"g++ -g -c $filename >> makefile
+        echo -n -e "\t"$compiler -g -c $filename >> makefile
     else
-        echo -n -e "\t"g++ -c $filename >> makefile
+        echo -n -e "\t"$compiler -c $filename >> makefile
     fi
     echo -n -e "\n" >> makefile
-    if [ "$verbose" = "true" ]
-    then
-        echo -n "."
-    fi
 done
 
 if [ "$debug" = "true" ]
 then
     echo -e "\n"debug : >> makefile
-    echo -e "\t"gdb $programname.x >> makefile
+    echo -e "\t"gdb $programname >> makefile
 fi
 
 if [ "$minimal" != "true" ]
@@ -155,19 +163,19 @@ then
 
 
     echo run : >> makefile
-    echo -e -n "\t"./$programname.x"\n" >> makefile
+    echo -e -n "\t"./$programname"\n" >> makefile
 fi
 
 if [ "$compile" = "true" ]
 then
     if [ "$verbose" = "true" ]
     then
-        echo -e "\nCompiling executable..."
+        echo -e "Compiling executable"
     fi
     echo -e make | sh
 fi
 
 if [ "$verbose" = "true" ]
 then
-    echo -e ".\nFinished!"
+    echo -e "Finished!"
 fi
